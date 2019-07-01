b211e167d0f69535c1a343c17032dbdc93001d7a
Matt Willson <mattjameswillson@gmail.com>
2019-06-30 21:09:35 -0400

    Move App to components directory

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import './custom.scss';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
