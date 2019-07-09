709be294c1a5c4cd1d38d826bbadb573013691f2
Matt Willson <mattjameswillson@gmail.com>
2019-07-09 17:48:53 -0400

    Convert to class component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">App-header</header>
-    </div>
-  );
-};
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">App-header</header>
+      </div>
+    );
+  }
+}
 
 export default App;
