bed812ecd9731b3afff51614b0ce57bee7bac7d2
Matt Willson <mattjameswillson@gmail.com>
2019-07-10 02:52:36 -0400

    Import YouTubeIcon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './Header.css';
+import YouTubeIcon from './YouTubeIcon';
 
 class Header extends React.Component {
   render() {
     return (
       <header className="Header">
         <h1>LFC Feed</h1>
+        <YouTubeIcon />
       </header>
     );
   }
