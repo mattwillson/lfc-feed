986a18cf054603ddae3c0cd37f06a19221ad5490
Matt Willson <mattjameswillson@gmail.com>
2019-07-10 15:49:40 -0400

    Create FeedItem component

---
+++ src/components/FeedItem.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class FeedItem extends React.Component {
+  render() {
+    return (
+      <div className="FeedItem">
+        <p>FeedItem</p>
+      </div>
+    );
+  }
+}
+
+export default FeedItem;
