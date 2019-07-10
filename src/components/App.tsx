53891a94a3ed389327afdbe11e4e0d10bbec977c
Matt Willson <mattjameswillson@gmail.com>
2019-07-10 02:16:22 -0400

    Import Feed component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import Header from './Header';
+import Feed from './Feed';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <Feed />
       </div>
     );
   }
