dcb739754f575a76401f49a424c294c812431e3e
Matt Willson <mattjameswillson@gmail.com>
2019-07-10 01:33:29 -0400

    Import Header

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
+import Header from './Header';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">App-header</header>
+        <Header />
       </div>
     );
   }
