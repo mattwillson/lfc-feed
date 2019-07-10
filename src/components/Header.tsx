b9a37deda3de0601d7107b46bb46c7fee135a142
Matt Willson <mattjameswillson@gmail.com>
2019-07-10 01:30:49 -0400

    Create Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header className="Header">
+        <h1>LFC Feed</h1>
+      </header>
+    );
+  }
+}
+
+export default Header;
