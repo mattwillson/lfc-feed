ad3d95e51a6ecc86c41c825d3aa665e5388718bd
Matt Willson <mattjameswillson@gmail.com>
2019-07-31 15:46:56 -0400

    Updates

---
+++ precache-manifest.4be2b7d208b1d428f44a03c6b116162d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4896328e763fa6a776ea6ac415fff1f6",
+    "url": "/lfc-feed/index.html"
+  },
+  {
+    "revision": "30062fdd3cede0e7b4b5",
+    "url": "/lfc-feed/static/css/main.8d0e0daa.chunk.css"
+  },
+  {
+    "revision": "d3e719139eff31635fb8",
+    "url": "/lfc-feed/static/js/2.a175895c.chunk.js"
+  },
+  {
+    "revision": "30062fdd3cede0e7b4b5",
+    "url": "/lfc-feed/static/js/main.18b43b66.chunk.js"
+  },
+  {
+    "revision": "d7afc90372ef9b27a955",
+    "url": "/lfc-feed/static/js/runtime~main.e23370a2.js"
+  },
+  {
+    "revision": "01c636fb044dbeb130a57b033a495a6f",
+    "url": "/lfc-feed/static/media/AlbertusMTPro.01c636fb.eot"
+  },
+  {
+    "revision": "1b823cc4ad2a9b69ecc6f68af04d0752",
+    "url": "/lfc-feed/static/media/AlbertusMTPro.1b823cc4.woff"
+  },
+  {
+    "revision": "b462e6945c1a50748f0ccfe519d179c5",
+    "url": "/lfc-feed/static/media/AlbertusMTPro.b462e694.ttf"
+  },
+  {
+    "revision": "ded4d968a195c4c226dfddced213706b",
+    "url": "/lfc-feed/static/media/AlbertusMTPro.ded4d968.woff2"
+  }
+]);
