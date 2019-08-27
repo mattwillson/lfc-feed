3e5dcee0bf07bd413ee3d8cd1fd49b7af419022a
Matt Willson <mattjameswillson@gmail.com>
2019-08-27 18:25:03 -0400

    Merge FeedItemMobile and FeedItemDesktop components

--- src/components/FeedItem.tsx
+++ src/components/FeedItem.tsx
@@ -1,33 +1,96 @@
-import React from 'react';
-import MediaQuery from 'react-responsive';
-import FeedItemMobile from './FeedItemMobile';
-import FeedItemDesktop from './FeedItemDesktop';
+import React, { useState } from 'react';
+import moment from 'moment';
+import { Card, Collapse, ResponsiveEmbed } from 'react-bootstrap';
+import './FeedItem.css';
+import youtube from '../api/youtube';
+import FeedModal from './FeedModal';
 
-type Props = {
-  video: any;
-  videoId: string;
+type Props = { video: any; videoId: string };
+
+const FeedItem = ({ video, videoId }: Props): JSX.Element => {
+  const [openCard, setOpen] = useState(false);
+
+  const [showModal, setShow] = useState(false);
+
+  const [views, setViews] = useState(0);
+
+  const videoSrc: string = `https://www.youtube.com/embed/${videoId}?modestbranding=1&rel=0`;
+
+  const getViewCount: () => Promise<void> = async () => {
+    const response = await youtube.get('/videos', {
+      params: {
+        playlistId: 'UU9LQwHZoucFT94I2h6JOcjw',
+        part: 'statistics',
+        id: videoId
+      }
+    });
+
+    setViews(response.data.items[0].statistics.viewCount);
+  };
+
+  const handleClick: () => void = () => {
+    if (window.matchMedia('(max-width: 575.98px)').matches) {
+      if (!openCard) {
+        getViewCount();
+      }
+      setOpen(!openCard);
+    } else {
+      setShow(!showModal);
+    }
+  };
+
+  return (
+    <>
+      <button className="FeedItem" onClick={handleClick}>
+        <Card>
+          <Collapse in={!openCard}>
+            <div>
+              <Card.Img
+                onClick={handleClick}
+                src={video.snippet.thumbnails.medium.url}
+              />
+              <Card.ImgOverlay>
+                <Card.Text>{video.snippet.title}</Card.Text>
+              </Card.ImgOverlay>
+            </div>
+          </Collapse>
+          <Collapse in={openCard} mountOnEnter={true} unmountOnExit={true}>
+            <div>
+              <ResponsiveEmbed aspectRatio="16by9">
+                <iframe src={videoSrc} title="video player" allowFullScreen />
+              </ResponsiveEmbed>
+              <Card.Body onClick={handleClick}>
+                <Card.Title>{video.snippet.title}</Card.Title>
+                <Card.Text>
+                  <span>
+                    {views.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}{' '}
+                    views
+                  </span>
+                </Card.Text>
+              </Card.Body>
+            </div>
+          </Collapse>
+          <Card.Footer onClick={handleClick}>
+            <time
+              dateTime={video.snippet.publishedAt}
+              title={moment(video.snippet.publishedAt).format('ll')}
+            >
+              {moment(video.snippet.publishedAt).fromNow()}
+            </time>
+          </Card.Footer>
+        </Card>
+      </button>
+
+      <FeedModal
+        show={showModal}
+        onHide={(): void => {
+          setShow(!showModal);
+        }}
+        video={video}
+        videoId={videoId}
+      />
+    </>
+  );
 };
-
-class FeedItem extends React.Component<Props, {}> {
-  render() {
-    return (
-      <>
-        <MediaQuery maxWidth={575.98}>
-          <FeedItemMobile
-            video={this.props.video}
-            videoId={this.props.videoId}
-          />
-        </MediaQuery>
-
-        <MediaQuery minWidth={576}>
-          <FeedItemDesktop
-            video={this.props.video}
-            videoId={this.props.videoId}
-          />
-        </MediaQuery>
-      </>
-    );
-  }
-}
 
 export default FeedItem;
