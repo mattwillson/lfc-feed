c43407e9d0d60627f6485df50b7bf64db5f51232
Matt Willson <mattjameswillson@gmail.com>
2019-09-16 15:30:56 -0400

    Incorporate youtube-iframe package

--- src/components/VideoEmbed.tsx
+++ src/components/VideoEmbed.tsx
@@ -1,20 +1,22 @@
 import React from 'react';
+import YouTubeIframe from 'youtube-iframe';
 import { ResponsiveEmbed } from 'react-bootstrap';
-import YouTube from 'react-youtube';
 
 type Props = { videoId: string };
 
 const VideoEmbed = ({ videoId }: Props) => {
-  const opts: any = {
-    playerVars: {
-      modestbranding: 1,
-      rel: 0
-    }
-  };
+  YouTubeIframe.load(function(YT) {
+    console.log(new YT.Player('player'));
+  });
 
   return (
     <ResponsiveEmbed className="VideoEmbed" aspectRatio="16by9">
-      <YouTube videoId={videoId} opts={opts} />
+      <iframe
+        id="player"
+        title="video player"
+        src={`https://www.youtube.com/embed/${videoId}?modestbranding=1&rel=0&enablejsapi=1`}
+        allowFullScreen
+      ></iframe>
     </ResponsiveEmbed>
   );
 };
