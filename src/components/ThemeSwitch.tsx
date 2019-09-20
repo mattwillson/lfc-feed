9325dec44107bfaedbbea8c3eaec6e209c3d77f7
Matt Willson <mattjameswillson@gmail.com>
2019-09-20 18:27:35 -0400

    Change ThemeSwitch track color to black

--- src/components/ThemeSwitch.tsx
+++ src/components/ThemeSwitch.tsx
@@ -1,11 +1,36 @@
 import React, { useContext } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import Switch from '@material-ui/core/Switch';
 import { ThemeContext } from '../theme-context';
 
 const ThemeSwitch = () => {
   const { toggleTheme } = useContext(ThemeContext);
 
-  return <Switch onChange={toggleTheme} color="default" />;
+  const useStyles = makeStyles({
+    switchBase: {
+      '&$checked + $track': {
+        opacity: 1
+      }
+    },
+    checked: {},
+    track: {
+      opacity: 1
+    }
+  });
+
+  const classes = useStyles();
+
+  return (
+    <Switch
+      onChange={toggleTheme}
+      classes={{
+        switchBase: classes.switchBase,
+        checked: classes.checked,
+        track: classes.track
+      }}
+      color="default"
+    />
+  );
 };
 
 export default ThemeSwitch;
