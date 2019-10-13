bc0c7c36a10c1fd05d2a9a6fa8d133ad5ad11be4
Matt Willson <mattjameswillson@gmail.com>
2019-10-13 17:31:50 -0400

    Remove youtube-iframe function

--- src/components/VideoEmbed.tsx
+++ src/components/VideoEmbed.tsx
@@ -1,14 +1,9 @@
 import React from 'react';
-import YouTubeIframe from 'youtube-iframe';
 import { ResponsiveEmbed } from 'react-bootstrap';
 
 type Props = { videoId: string };
 
 const VideoEmbed = ({ videoId }: Props) => {
-  YouTubeIframe.load(function(YT) {
-    console.log(new YT.Player('player'));
-  });
-
   return (
     <ResponsiveEmbed className="VideoEmbed" aspectRatio="16by9">
       <iframe
